b462e935464a77b1fc44d4f67c8bd7c475a57974
Hari8878 <94231622+Hari8878@users.noreply.github.com>
2026-08-10 21:47:28 +0530

    Refactor vehicle function and remove home route
    Removed the home route and unnecessary headers from the vehicle function.

--- main.py
+++ main.py
@@ -5,14 +5,6 @@ app = Flask(__name__)
 
 API_URL = "https://horizon.policyboss.com:5443/quote/pb_vehicle_info"
 
-
-@app.get("/")
-def home():
-    return jsonify({
-        "success": True,
-        "message": "Flask API is running"
-    })
-    
 @app.get("/Vehicle")
 def vehicle():
     reg_value = request.args.get("reg_value")
@@ -36,28 +28,10 @@ def vehicle():
   "g-recaptcha-response": token_value,
   "captcha": token_value
 }
-headers = {
-  'Host': "horizon.policyboss.com:5443",
-  'User-Agent': "Mozilla/5.0 (Linux; Android 15; V2250 Build/AP3A.240905.015.A2; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/152.0.7977.30 Safari/537.36",
-  'Accept': "application/json, text/javascript, */*; q=0.01",
-  'Accept-Encoding': "gzip, deflate, br, zstd",
-  'sec-ch-ua-platform': "\"Android\"",
-  'sec-ch-ua': "\"Chromium\";v=\"152\", \"Not?A_Brand\";v=\"24\", \"Android WebView\";v=\"152\"",
-  'content-type': "application/json;charset=UTF-8",
-  'sec-ch-ua-mobile': "?1",
-  'origin': "https://www.policyboss.com",
-  'x-requested-with': "mark.via.gp",
-  'sec-fetch-site': "same-site",
-  'sec-fetch-mode': "cors",
-  'sec-fetch-dest': "empty",
-  'referer': "https://www.policyboss.com/",
-  'accept-language': "en-US,en;q=0.9",
-  'priority': "u=1, i"
-}
 
     response = requests.post(
         API_URL,
-        json=payload,headers=headers,
+        json=payload,
         timeout=20
     )
 
